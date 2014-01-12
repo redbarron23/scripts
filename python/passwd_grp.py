7f9e10edd1deb32cff174f0883de0d7fb87a96db
jaime hourihane <jhourihane@gmail.com>
2014-01-11 21:50:41 -0800

    if condition to make sure groupname does not equal username. This would be redundant

--- python/passwd_grp.py
+++ python/passwd_grp.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
-
 import pwd, grp
+
 for p in pwd.getpwall():
-    print p[0], grp.getgrgid(p[3])[0]
+    if p[0] == grp.getgrgid(p[3])[0]:
+       pass
+    else:
+        print p[0], grp.getgrgid(p[3])[0]
