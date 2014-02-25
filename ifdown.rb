68e13e6f43d45ed513b9f51a18af0e03c2c6c452
jaime hourihane <jhourihane@gmail.com>
2014-02-24 22:19:23 -0800

    untested rake script for puppet

--- ifdown.rb
+++ ifdown.rb
@@ -1,2 +1,13 @@
 #!/usr/bin/env ruby
-#
+puts "Shutting down VirtualBox interfaces...."#
+
+#range = (1..9).to_a
+system('/sbin/ifconfig', 'vboxnet0', 'down')
+system('/sbin/ifconfig', 'vboxnet1', 'down')
+system('/sbin/ifconfig', 'vboxnet2', 'down')
+system('/sbin/ifconfig', 'vboxnet3', 'down')
+system('/sbin/ifconfig', 'vboxnet4', 'down')
+system('/sbin/ifconfig', 'vboxnet5', 'down')
+system('/sbin/ifconfig', 'vboxnet6', 'down')
+system('/sbin/ifconfig', 'vboxnet7', 'down')
+system('/sbin/ifconfig', 'vboxnet8', 'down')
